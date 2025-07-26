1681832dd9ffdc21581c4434c57c254b132708ed
rofergon <sebas.2023@gmail.com>
2025-07-26 15:26:07 -0500

    Add DeFi tools export module for Bonzo Finance. This new file consolidates exports for Bonzo-related tools, enhancing modularity and preparing for future protocol integrations. Comments indicate potential future exports for Uniswap, Saucerswap, and Pangolin.

---
+++ typescript/src/shared/tools/defi/index.ts
@@ -0,0 +1,7 @@
+// Export all Bonzo Finance tools
+export * from './bonzo';
+
+// Future DeFi protocol exports:
+// export * from './uniswap';
+// export * from './saucerswap';
+// export * from './pangolin'; 
