a959e6c637a87ec509657f2276749219c52b6b55
rofergon <sebas.2023@gmail.com>
2025-07-30 14:38:31 -0500

    Export SaucerSwap Finance tools in the shared DeFi module, enhancing the integration of DEX capabilities. This update includes the addition of the SaucerSwap export, previously commented out, to streamline access to its functionalities.

--- typescript/src/shared/tools/defi/index.ts
+++ typescript/src/shared/tools/defi/index.ts
@@ -1,7 +1,9 @@
 // Export all Bonzo Finance tools
 export * from './bonzo';
 
+// Export all SaucerSwap Finance tools
+export * from './saucerswap';
+
 // Future DeFi protocol exports:
 // export * from './uniswap';
-// export * from './saucerswap';
 // export * from './pangolin'; 
