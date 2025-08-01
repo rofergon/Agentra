3c68de7df1991195cc2fdc44ce468dac0f23d5dc
rofergon <sebas.2023@gmail.com>
2025-08-01 12:33:25 -0500

    Export SaucerSwap Router swap quote tools in the shared DeFi module, enhancing the integration of contract-based swap functionalities. This update improves access to real-time swap quotes and expands the DeFi toolset.

--- typescript/src/shared/tools/defi/index.ts
+++ typescript/src/shared/tools/defi/index.ts
@@ -4,6 +4,9 @@ export * from './bonzo';
 // Export all SaucerSwap Finance tools
 export * from './saucerswap';
 
+// Export SaucerSwap Router swap quote tools
+export * from './saucerswap-swap';
+
 // Future DeFi protocol exports:
 // export * from './uniswap';
 // export * from './pangolin'; 
